ebd41ed186e82980da2fb8752ee4d36ffae1906a
yersel500 <yersel500@gmail.com>
2022-08-05 23:37:29 -0500

    add itemBought feature

--- cloudFunctions/updateActiveItems.js
+++ cloudFunctions/updateActiveItems.js
@@ -51,3 +51,33 @@ Moralis.Cloud.afterSave("ItemCanceled", async (request) => {
     }
   }
 });
+
+Moralis.Cloud.afterSave("ItemBought", async (request) => {
+  const confirmed = request.object.get("confirmed");
+  const logger = Moralis.Cloud.getLogger();
+  logger.info(`Marketplace | Object: ${request.object}`);
+  if (confirmed) {
+    const ActiveItem = Moralis.Object.extend("ActiveItem");
+    const query = new Moralis.Query(ActiveItem);
+    query.equalTo("marketplaceAddress", request.object.get("address"));
+    query.equalTo("nftAddress", request.object.get("nftAddress"));
+    query.equalTo("tokenId", request.object.get("tokenId"));
+    logger.info(`Marketplace | Query: ${query}`);
+    const boughtItem = await query.first();
+    if (boughtItem) {
+      logger.info(`Deleting ${request.object.get("objectId")}`);
+      await boughtItem.destroy();
+      logger.info(
+        `Deleted item with Token ID ${request.object.get(
+          "tokenId"
+        )} at address ${request.object.get("address")}`
+      );
+    } else {
+      logger.info(
+        `No item found with address ${request.object.get(
+          "address"
+        )} and tokenId: ${request.object.get("tokenId")} `
+      );
+    }
+  }
+});
