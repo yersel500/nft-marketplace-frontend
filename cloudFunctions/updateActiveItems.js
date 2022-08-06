115576693ad9a2076f073acb8b308b177fdf1626
yersel500 <yersel500@gmail.com>
2022-08-05 21:53:19 -0500

    Update item canceled

--- cloudFunctions/updateActiveItems.js
+++ cloudFunctions/updateActiveItems.js
@@ -21,3 +21,33 @@ Moralis.Cloud.afterSave("ItemListed", async (request) => {
     await activeItem.save();
   }
 });
+
+Moralis.Cloud.afterSave("ItemCanceled", async (request) => {
+  const confirmed = request.object.get("confirmed");
+  const logger = Moralis.Cloud.getLogger();
+  logger.info(`Marketplace | Object: ${request.object}`);
+  if (confirmed) {
+    const ActiveItem = Moralis.Object.extend("ActiveItem");
+    const query = new Moralis.Query(ActiveItem);
+    query.equalTo("marketplaceAddress", request.object.get("address"));
+    query.equalTo("nftAddress", request.object.get("nftAddress"));
+    query.equalTo("tokenId", request.object.get("tokenId"));
+    logger.info(`Marketplace | Query: ${query}`);
+    const canceledItem = await query.first();
+    logger.info(`Marketplace | CanceledItem: ${JSON.stringify(canceledItem)}`);
+    if (canceledItem) {
+      logger.info(
+        `Deleting ${request.object.get(
+          "tokenId"
+        )} at address ${request.object.get("address")} since it was canceled`
+      );
+      await canceledItem.destroy();
+    } else {
+      logger.info(
+        `No item found with address ${request.object.get(
+          "address"
+        )} and tokenId: ${request.object.get("tokenId")} `
+      );
+    }
+  }
+});
