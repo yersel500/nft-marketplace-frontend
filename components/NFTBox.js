3af77428222a099e3d320be3640eb9a46aa2b520
yersel500 <yersel500@gmail.com>
2022-08-07 12:13:56 -0500

    buy item feature

--- components/NFTBox.js
+++ components/NFTBox.js
@@ -3,8 +3,23 @@ import { useWeb3Contract, useMoralis } from "react-moralis";
 import nftMarketplaceAbi from "../constants/NftMarketplace.json";
 import nftAbi from "../constants/BasicNft.json";
 import Image from "next/image";
-import { Card } from "@web3uikit/core";
+import { Card, useNotification } from "@web3uikit/core";
 import { ethers } from "ethers";
+import UpdateListingModal from "./UpdateListingModal";
+
+const truncateStr = (fullStr, strLen) => {
+  if (fullStr.length <= strLen) return fullStr;
+  const separator = "...";
+  const separatorLength = separator.length;
+  const charsToShow = strLen - separatorLength;
+  const frontChars = Math.ceil(charsToShow / 2);
+  const backChars = Math.floor(charsToShow / 2);
+  return (
+    fullStr.substring(0, frontChars) +
+    separator +
+    fullStr.substring(fullStr.length - backChars)
+  );
+};
 
 export default function NFTBox({
   price,
@@ -13,16 +28,30 @@ export default function NFTBox({
   marketplaceAddress,
   seller,
 }) {
-  const { isWeb3Enabled } = useMoralis();
+  const { isWeb3Enabled, account } = useMoralis();
   const [imageURI, setImageURI] = useState("");
   const [tokenName, setTokenName] = useState("");
   const [tokenDescription, setTokenDescription] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const hideModal = () => setShowModal(false);
+  const dispatch = useNotification();
 
   const { runContractFunction: getTokenURI } = useWeb3Contract({
     abi: nftAbi,
     contractAddress: nftAddress,
     functionName: "tokenURI",
     params: {
+      tokenId: tokenId,
+    },
+  });
+
+  const { runContractFunction: buyItem } = useWeb3Contract({
+    abi: nftMarketplaceAbi,
+    contractAddress: marketplaceAddress,
+    functionName: "buyItem",
+    msgValue: price,
+    params: {
+      nftAddress: nftAddress,
       tokenId: tokenId,
     },
   });
@@ -47,27 +76,65 @@ export default function NFTBox({
     }
   }, [isWeb3Enabled]);
 
+  const isOwnedByUser = seller === account || seller === undefined;
+  const formattedSellerAddress = isOwnedByUser
+    ? "you"
+    : truncateStr(seller || "", 15);
+
+  const handleCardClick = () => {
+    isOwnedByUser
+      ? setShowModal(true)
+      : buyItem({
+          onError: (error) => console.log(error),
+          onSuccess: () => handleBuyItemSuccess(),
+        });
+  };
+
+  const handleBuyItemSuccess = () => {
+    dispatch({
+      type: "success",
+      message: "Item bought!",
+      title: "Item Bought",
+      position: "topR",
+    });
+  };
+
   return (
     <div>
       <div>
         {imageURI ? (
-          <Card title={tokenName} description={tokenDescription}>
-            <div className="p-2">
-              <div className="flex flex-col items-end gap-2">
-                <div>#{tokenId}</div>
-                <div className="italic text-sm">Owned by {seller}</div>
-                <Image
-                  loader={() => imageURI}
-                  src={imageURI}
-                  height="200"
-                  width="200"
-                />
-                <div className="font-bold">
-                  {ethers.utils.formatUnits(price, "ether")} ETH
+          <div>
+            <UpdateListingModal
+              isVisible={showModal}
+              tokenId={tokenId}
+              marketplaceAddress={marketplaceAddress}
+              nftAddress={nftAddress}
+              onClose={hideModal}
+            />
+            <Card
+              title={tokenName}
+              description={tokenDescription}
+              onClick={handleCardClick}
+            >
+              <div className="p-2">
+                <div className="flex flex-col items-end gap-2">
+                  <div>#{tokenId}</div>
+                  <div className="italic text-sm">
+                    Owned by {formattedSellerAddress}
+                  </div>
+                  <Image
+                    loader={() => imageURI}
+                    src={imageURI}
+                    height="200"
+                    width="200"
+                  />
+                  <div className="font-bold">
+                    {ethers.utils.formatUnits(price, "ether")} ETH
+                  </div>
                 </div>
               </div>
-            </div>
-          </Card>
+            </Card>
+          </div>
         ) : (
           <div>Loading...</div>
         )}
